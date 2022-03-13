7f3de0b577eddc3d456d2047c3fcced6e54c8ae7
Amy Young <youngamy1223@gmail.com>
2022-03-13 14:57:44 -0600

    update files for migration

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -1,17 +1,16 @@
-
 using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
   public class Category
-  {
-    public Category()
     {
-      this.Items = new HashSet<Item>();
-    }
+        public Category()
+        {
+            this.JoinEntities = new HashSet<CategoryItem>();
+        }
 
-    public int CategoryId { get; set; }
-    public string Name { get; set; }
-    public virtual ICollection<Item> Items { get; set; }
-  }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<CategoryItem> JoinEntities { get; set; }
+    }
 }
